cd4d63f596ece4d039b3836fa3ec598a80a65691
Erik Moroz <erik@moroz.uk>
2026-03-23 21:48:21 +0100

    refactor: move preferences validation to Pydantic schema level

--- backend/users/services.py
+++ backend/users/services.py
@@ -10,7 +10,6 @@ from users.exceptions import (
     UserDeletionBlockedError,
     UserInvalidConsentTypeError,
     UserInvalidPasswordError,
-    UserValidationError,
 )
 from users.models import ConsentType, FontChoices, User, UserConsent, UserPreferences, WeekdayChoices
 
@@ -35,14 +34,9 @@ class UserService:
         preferences = UserService.get_or_create_preferences(user)
 
         if data.calendar_start_day is not None:
-            if data.calendar_start_day < 1 or data.calendar_start_day > 7:
-                raise UserValidationError('calendar_start_day must be between 1 and 7')
             preferences.calendar_start_day = data.calendar_start_day
 
         if data.font_family is not None:
-            valid_fonts = [choice[0] for choice in FontChoices.choices]
-            if data.font_family not in valid_fonts:
-                raise UserValidationError(f'font_family must be one of: {", ".join(valid_fonts)}')
             preferences.font_family = data.font_family
 
         preferences.save()
